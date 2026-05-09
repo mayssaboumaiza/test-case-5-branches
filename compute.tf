2dd047e37e88edc3807c5d97508f1bae8ee4d4c4
mayssa boumaiza <mayssaboumaiza2002@gmail.com>
2026-05-09 23:55:36 +0100

    feat: staging environment config

--- compute.tf
+++ compute.tf
@@ -1,1 +1,1 @@
-resource "aws_instance" "web" { ami = "ami-0c02fb55956c7d316"; instance_type = "t3.large"; tags = { Env = var.environment } }
+resource "aws_instance" "web" { ami = "ami-0c02fb55956c7d316"; instance_type = "t3.small"; tags = { Env = "staging" } }
